cfe2c12b09d01871eb3479bcb971e256b6f4f58c
rinne9029 <0kt4x34y2ky637r@au.com>
2022-11-14 10:00:38 +0900

    Mapを分けた

--- Game03/Project/GameProject/Game/Game.cpp
+++ Game03/Project/GameProject/Game/Game.cpp
@@ -1,40 +1,16 @@
 #include"Game.h"
+#include"GameData.h"
 #include"Field.h"
 #include"Player.h"
 #include"Enemy.h"
 #include"Goal.h"
-#include"Start.h"
-#include"Signboard.h"
 #include"Map.h"
-#include"Fallingfloor.h"
-#include"FireTrap.h"
 #include"../Title/Title.h"
 
 Game::Game() :Base(eType_Scene)
 {
 	Base::Add(new Field());
-	Base::Add(new Map());
-	//プレイヤー配置
-	Base::Add(new Player(CVector2D(MAP_TIP_SIZE * 3, MAP_TIP_SIZE * 20), false));
-
-	//敵配置
-	//Base::Add(new Enemy(CVector2D(1280 + 256 * 1, 540), true));
-	//Base::Add(new Enemy(CVector2D(1280 + 256 * 2, 540), true));
-	//Base::Add(new Enemy(CVector2D(1280 + 256 * 3, 540), true));
-
-	//ゴール配置
-	Base::Add(new Goal(CVector2D(MAP_TIP_SIZE * 9, MAP_TIP_SIZE * 18)));
-
-	//スタート配置
-	Base::Add(new Start(CVector2D(MAP_TIP_SIZE * 3, MAP_TIP_SIZE * 21)));
-
-	//看板配置
-	Base::Add(new Signboard(CVector2D(MAP_TIP_SIZE * 6, MAP_TIP_SIZE * 21)));
-	//落ちる床配置
-	//Base::Add(new Fallingfloor(CVector2D(MAP_TIP_SIZE * 10, MAP_TIP_SIZE * 17)));
-
-	//火柱の配置
-	//Base::Add(new FireTrap(CVector2D(MAP_TIP_SIZE * 12, MAP_TIP_SIZE * 17),false));
+	Base::Add(new Map(GameData::stage));
 
 
 
